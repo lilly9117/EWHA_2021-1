36ed90af1befeae044c4d4b89edcee256800553a
lilly9117 <lilly9117@naver.com>
2021-03-30 01:46:20 +0900

    modify code

--- Bayesian_Statistics/hw2_prob_7.R
+++ Bayesian_Statistics/hw2_prob_7.R
@@ -6,6 +6,11 @@ legend("topright",c("B(10,0.7)", "N(7,2.1)"), lty=1:2)
 title(main = 'Probability density function', xlab = "x", ylab = "f(x)")
 
 
-
+x<- seq(20,100)
+plot(x, dbinom(x,100,0.7), type = 'l', ann = FALSE)
+y<- seq(20,100,by=0.01)
+lines(y, dnorm(y,mean = 100*0.7, sd = sqrt(100*0.7*0.3)), lty=2, col = "red")
+legend("topright",c("B(100,0.7)", "N(7,21)"), lty=1:2)
+title(main = 'Probability density function', xlab = "x", ylab = "f(x)")
 
 
