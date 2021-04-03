fca4fbdb66156165301f630082bb7718941f9773
lilly9117 <lilly9117@naver.com>
2021-04-03 16:53:17 +0900

    modify code

--- Time_series/hw1_2.R
+++ Time_series/hw1_2.R
@@ -3,13 +3,13 @@ set.seed(1)
 
 n = 1000
 a = rnorm(n, mean = 0, sd = 5)
-y = rep(n,0)
+y = rep(n+1,0)
 mu = 10
 y[1] = mu
 
 phi = -0.8
-for (t in 2:n){
-  y[t] = mu + phi*(y[t-1]-mu) + a[t]
+for (t in 1:n){
+  y[t+1] = mu + phi*(y[t]-mu) + a[t]
 }
 plot(y,type="o", ann = FALSE)
 title(main = 'phi = -0.8', ylab = 'yt')
@@ -27,13 +27,13 @@ set.seed(1)
 
 n = 1000
 a = rnorm(n, mean = 0, sd = 5)
-y = rep(n,0)
+y = rep(n+1,0)
 mu = 10
 y[1] = mu
 
 phi = 0.8
-for (t in 2:n){
-  y[t] = mu + phi*(y[t-1]-mu) + a[t]
+for (t in 1:n){
+  y[t+1] = mu + phi*(y[t]-mu) + a[t]
 }
 plot(y,type="o", ann = FALSE)
 title(main = 'phi = 0.8', ylab = 'yt')
@@ -51,13 +51,13 @@ set.seed(1)
 
 n = 1000
 a = rnorm(n, mean = 0, sd = 5)
-y = rep(n,0)
+y = rep(n+1,0)
 mu = 10
 y[1] = mu
 
 phi = 1
-for (t in 2:n){
-  y[t] = mu + phi*(y[t-1]-mu) + a[t]
+for (t in 1:n){
+  y[t+1] = mu + phi*(y[t]-mu) + a[t]
 }
 plot(y,type="o", ann = FALSE)
 title(main = 'phi = 1', ylab = 'yt')
